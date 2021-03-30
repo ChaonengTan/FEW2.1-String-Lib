5ccc197fdfd0d10764ec6fa90b0f6da34e6a29da
Chaoneng Tan <69783753+ChaonengTan@users.noreply.github.com>
2021-03-29 18:49:54 -0700

    +done

--- src/index.js
+++ src/index.js
@@ -9,6 +9,12 @@ function lowerFirst(str) {
   var firstLetter = str[0].toLowerCase()
   return firstLetter + end
 }
+function removeElement(array, elem) {
+  var index = array.indexOf(elem);
+  if (index > -1) {
+    array.splice(index, 1);
+  }
+}
 // Assignment
 function capitalize(str){
   return upperFirst(str)
@@ -26,24 +32,16 @@ function capitalizeWords(str) {
 }
 function removeExtraSpaces(str){
   var words = str.split(' ')
-  var list = []
-  list.push(words[0].toLowerCase())
-  for (var i = 1; i < words.length; i++) {
-    list.push(upperFirst(words[i]))
+  console.log(words)
+  for (var i = words.length; i > 1; i--) {
+    if (words[i]==''){
+      removeElement(words, words[i])
+    }
   }
-  return list.join('')
+  return words.join(' ')
 }
-function camelCase(str) {
-  var words = str.split(' ')
-  var list = []
-  list.push(words[0].toLowerCase())
-  for (var i = 1; i < words.length; i++) {
-    list.push(upperFirst(words[i]))
-  }
-  return list.join('')
-}
-function kebabCase(str, sep = '-') {
-  var words = str.split(' ')
+function kebabCase(str) {
+  var words = removeExtraSpaces(str).split(' ')
   var list = []
   for (var i = 0; i < words.length; i++) {
     list.push(lowerFirst(words[i]))
@@ -51,10 +49,40 @@ function kebabCase(str, sep = '-') {
   return list.join('-')
 }
 function snakeCase(str) {
-  var words = str.split(' ')
+  var words = removeExtraSpaces(str).split(' ')
   var list = []
   for (var i = 0; i < words.length; i++) {
     list.push(lowerFirst(words[i]))
   }
   return list.join('_')
 }
+function camelCase(str) {
+  var words = removeExtraSpaces(str).split(' ')
+  var list = []
+  list.push(words[0].toLowerCase())
+  for (var i = 1; i < words.length; i++) {
+    list.push(upperFirst(words[i]))
+  }
+  return list.join('')
+}
+function shift(str, index=1) {
+  var end = str.slice(index)
+  var firstLetter = str[0]
+  return end+firstLetter
+}
+// create Func
+console.log('running func')
+function makeHashTag(str) {
+  var words = removeExtraSpaces(str).split(' ')
+  var list = []
+  if (words.length>3){
+    console.log(words)
+    words.sort((a,b) => b.length - a.length);
+    console.log(words)
+    for (var i = 0; i < words.length; i++) {
+      list.push(upperFirst(words[i]))
+    }
+  }
+  return list.join('')
+}
+console.log(makeHashTag('hello world foo   aaaaaaaaaa    bar!'))

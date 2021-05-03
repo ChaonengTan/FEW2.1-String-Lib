78085ab5cf8c02d912c8a3428c59609301e78e6e
ChaonengTan <chaonengtan@gmail.com>
2021-05-03 16:10:17 -0700

    +moreTS

--- src/indexTS.ts
+++ src/indexTS.ts
@@ -1,110 +1,110 @@
 /* eslint linebreak-style: ["error", "windows"] */
 // handyFunctions
-function upperFirst(str: string) {
+function upperFirst(str: string): string {
   if (str === '') {
     return '';
   }
-  const end = str.slice(1);
-  const firstLetter = str[0].toUpperCase();
+  const end: string = str.slice(1);
+  const firstLetter: string = str[0].toUpperCase();
   return firstLetter + end;
 }
 function lowerFirst(str: string) {
   if (str === '') {
     return '';
   }
-  const end = str.slice(1);
-  const firstLetter = str[0].toLowerCase();
+  const end: string = str.slice(1);
+  const firstLetter: string = str[0].toLowerCase();
   return firstLetter + end;
 }
-function removeElement(array: string[], elem: string) {
+function removeElement(array: string[], elem: string): void {
   const index: number = array.indexOf(elem);
   if (index > -1) {
     array.splice(index, 1);
   }
 }
-function removeExtraSpaces(str: string) {
+function removeExtraSpaces(str: string): string {
   const words: string[] = str.trim().split(' ');
-  for (let i = words.length - 1; i > -1; i -= 1) {
+  for (let i: number = words.length - 1; i > -1; i -= 1) {
     if (words[i] === '') {
       removeElement(words, words[i]);
     }
   }
   return words.join(' ');
 }
-function ksCase(str: string, type: string) {
+function ksCase(str: string, type: string): string {
   if (str === '') {
     return '';
   }
-  const words = removeExtraSpaces(str).split(' ');
-  const list = [];
-  for (let i = 0; i < words.length; i += 1) {
+  const words: string[] = removeExtraSpaces(str).split(' ');
+  const list: string[] = [];
+  for (let i: number = 0; i < words.length; i += 1) {
     list.push(lowerFirst(words[i]));
   }
   return list.join(type);
 }
 // Assignment
-function capitalize(str: string) {
+function capitalize(str: string): string {
   return upperFirst(str);
 }
-function allCaps(str: string) {
+function allCaps(str: string): string {
   return str.toUpperCase();
 }
-function capitalizeWords(str: string) {
-  const words = str.split(' ');
-  const list = [];
-  for (let i = 0; i < words.length; i += 1) {
+function capitalizeWords(str: string): string {
+  const words: string[] = str.split(' ');
+  const list: string[] = [];
+  for (let i: number = 0; i < words.length; i += 1) {
     list.push(upperFirst(words[i]));
   }
   return list.join(' ');
 }
-function kebabCase(str: string) {
+function kebabCase(str: string): string {
   return ksCase(str, '-');
 }
-function snakeCase(str: string) {
+function snakeCase(str: string): string {
   return ksCase(str, '_');
 }
-function camelCase(str: string) {
+function camelCase(str: string): string {
   if (str === '') {
     return '';
   }
-  const words = removeExtraSpaces(str).split(' ');
+  const words: string[] = removeExtraSpaces(str).split(' ');
   const list: string[] = [];
   list.push(words[0].toLowerCase());
-  for (let i = 1; i < words.length; i += 1) {
+  for (let i: number = 1; i < words.length; i += 1) {
     list.push(upperFirst(words[i]));
   }
   return list.join('');
 }
-function shift(str: string, index = 1) {
+function shift(str: string, index = 1): string {
   if (str === '') {
     return '';
   }
-  const end = str.slice(index);
-  const firstLetter = str[0];
+  const end: string = str.slice(index);
+  const firstLetter: string = str[0];
   return end + firstLetter;
 }
-function makeHashTag(str: string) {
+function makeHashTag(str: string): string[] {
   if (str === '') {
     return [''];
   }
-  const words = removeExtraSpaces(str).split(' ');
+  const words: string[] = removeExtraSpaces(str).split(' ');
   const list: string[] = [];
   if (words.length > 3) {
     words.sort((a, b) => b.length - a.length);
-    for (let i = 0; i < 3; i += 1) {
+    for (let i: number = 0; i < 3; i += 1) {
       list.push(`#${upperFirst(words[i]).toString()}`);
     }
   } else {
     words.sort((a, b) => b.length - a.length);
-    for (let i = 0; i < words.length; i += 1) {
+    for (let i: number = 0; i < words.length; i += 1) {
       list.push(`#${upperFirst(words[i]).toString()}`);
     }
   }
   return list;
 }
-function isEmpty(str: string) {
-  const words = str.trim().split('');
-  for (let i = 0; i < words.length; i += 1) {
+function isEmpty(str: string): boolean {
+  const words: string[] = str.trim().split('');
+  for (let i: number = 0; i < words.length; i += 1) {
     if (words[i] !== ('\t' || '\r' || '\n')) {
       return false;
     }
